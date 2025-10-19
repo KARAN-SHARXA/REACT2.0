94334621393407ce10cb018fb7eda8f32b9f73ff
KARAN-SHARXA <karansharma2302578@gmail.com>
2025-10-19 11:00:57 +0530

    form handling

--- 09formhandling/src/App.jsx
+++ 09formhandling/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 function App() {
+
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    console.log('Form Submitted');
+    
+  }
   return (
-    <div>App</div>
+    <div>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }} >
+        <input type="text" name="" id="" placeholder='enter you name' />
+        <button>Submit</button>
+        
+      </form>
+    </div>
   )
 }
 
